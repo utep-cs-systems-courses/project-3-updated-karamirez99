0394480be4006e95b3fe892eb7b56da3b9f7d354
student <karamirez7@miners.utep.edu>
2020-11-22 18:06:36 -0700

    Implemented additional font sizes

--- lcdLib/lcddraw.c
+++ lcdLib/lcddraw.c
@@ -72,6 +72,49 @@ void drawChar5x7(u_char rcol, u_char rrow, char c,
   }
 }
 
+void drawChar8x12(u_char rcol, u_char rrow, char c,
+		  u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char col = 0;
+  u_char row = 0;
+  u_char bit = 0x80;
+  u_char oc = c - 0x20;
+
+lcd_setArea(rcol, rrow, rcol + 7, rrow + 11); /* relative to requested col/row */
+ while (col < 12) {
+    while (row < 8) {
+      u_int colorBGR = (font_8x12[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+      lcd_writeColor(colorBGR);
+      row++;
+      bit >>= 1;
+    }
+    col++;
+    bit = 0x80;
+    row = 0;
+  }
+}
+
+void drawChar11x16(u_char rcol, u_char rrow, char c,
+		   u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char col = 0;
+  u_char row = 0;
+  u_int bit = 0x01;
+  u_char oc = c - 0x20;
+
+  lcd_setArea(rcol, rrow, rcol + 10, rrow + 15);
+  while (row < 16) {
+    while (col < 11) {
+      u_int colorBGR = (font_11x16[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+      lcd_writeColor(colorBGR);
+      col++;
+    }
+    col = 0;
+    bit <<= 1;
+    row++;
+  }	
+}
+
 /** Draw string at col,row
  *  Type:
  *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
@@ -94,6 +137,25 @@ void drawString5x7(u_char col, u_char row, char *string,
   }
 }
 
+void drawString8x12(u_char col, u_char row, char *string,
+		     u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char cols = col;
+  while (*string) {
+    drawChar8x12(cols, row, *string++, fgColorBGR, bgColorBGR);
+    cols += 8;
+  }
+}
+
+void drawString11x16(u_char col, u_char row, char *string,
+		     u_int fgColorBGR, u_int bgColorBGR)
+{
+  u_char cols = col;
+  while (*string) {
+    drawChar11x16(cols, row, *string++, fgColorBGR, bgColorBGR);
+    cols += 11;
+  }
+}
 
 /** Draw rectangle outline
  *  
