5d283e02c85d0459030c0a1daee6a41f530fe7ef
student <karamirez7@miners.utep.edu>
2020-11-29 14:13:00 -0700

    Moving snake functionality

---
+++ project/src/p2_interrupt_handler.c
@@ -0,0 +1,12 @@
+#include <msp430.h>
+#include "switches.h"
+
+/* Our switches connecting to P2 */
+
+void __interrupt_vec(PORT2_VECTOR) Port_2(){
+
+  if (P2IFG & SWITCHES) {
+    P2IFG &= ~SWITCHES;
+    switch_interrupt_handler();
+  }
+}
