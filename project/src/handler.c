76d6ef05cd0ca5340d44d1cbc1cb953799139a37
student <karamirez7@miners.utep.edu>
2020-11-29 18:39:42 -0700

    Changed wdt handlers

---
+++ project/src/handler.c
@@ -0,0 +1,30 @@
+#include <msp430.h>
+//#include "stateMachine.h"
+#include "game.h"
+#include "led.h"
+
+//void __interrupt_vec(WDT_VECTOR) WDT(){
+void wdt_c_handler(){
+  static unsigned char timeAdvance = 0;
+   green_on = 1;
+    led_changed = 1;
+    led_update();
+  
+  if (gameOver){
+    end_game();
+    green_on = 0;
+    led_changed = 1;
+    led_update();
+    
+    and_sr(~0x8);
+    or_sr(0x10);
+  }
+  
+  if (++timeAdvance == 20) {
+    updateGame = 1;
+  }
+  
+   green_on = 0;
+   led_changed = 1;
+   led_update();
+}
--- project/src/p2_interrupt_handler.c
+++ project/src/p2_interrupt_handler.c
@@ -1,12 +1,21 @@
 #include <msp430.h>
 #include "switches.h"
+#include "led.h" 
 
 /* Our switches connecting to P2 */
 
 void __interrupt_vec(PORT2_VECTOR) Port_2(){
+  green_on = 1;
+  led_changed = 1;
+  led_update();
 
+  
   if (P2IFG & SWITCHES) {
     P2IFG &= ~SWITCHES;
     switch_interrupt_handler();
   }
+
+  green_on = 0;
+  led_changed = 1;
+  led_update();
 }
